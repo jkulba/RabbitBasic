ff78f094cc229085c2fb7fd8659818674e5899d3
James Kulba <jkulba@gmail.com>
2020-11-03 15:41:00 -0800

    Adjusted path for UNIX system

--- Send/Send.cs
+++ Send/Send.cs
@@ -9,16 +9,17 @@ namespace Send
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "192.168.86.246", UserName = "guest", Password = "guest"};
+            var factory = new ConnectionFactory() { HostName = "192.168.86.246", UserName = "guest", Password = "guest" };
             string message = "";
-            string jsonFilePath = @"C:\Users\jkulba\Projects\RabbitBasic\samples\ElectrodeTestRequest.json";
+            // string jsonFilePath = @"C:\Users\jkulba\Projects\RabbitBasic\samples\ElectrodeTestRequest.json";
+            string jsonFilePath = @"/home/jim/Projects/RabbitBasic/samples/ElectrodeTestRequest.json";
             using (StreamReader r = new StreamReader(jsonFilePath))
             {
                 message = r.ReadToEnd();
             }
 
             using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())  
+            using (var channel = connection.CreateModel())
             {
                 channel.QueueDeclare(queue: "CoreToProcessorImpedanceData", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
@@ -26,7 +27,7 @@ namespace Send
                 {
                     var body = Encoding.UTF8.GetBytes(message);
                     channel.BasicPublish(exchange: "HUv1Backend", routingKey: "CoreToProcessorImpedanceData", basicProperties: null, body: body);
-                    //Console.WriteLine(message);
+                    Console.WriteLine(i);
                 }
             }
             Console.WriteLine("Press [enter] to exit.");
